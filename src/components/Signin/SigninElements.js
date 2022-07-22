fec64e4d50183931858701f63a254b69e41f8f5c
Yasser Emam <YasserEmam2002@gmail.com>
2022-07-22 03:26:45 +0200

    stoppend while developing sing in page

---
+++ src/components/Signin/SigninElements.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const SinginLog = styled.h1`
+  color: #fff;
+  font-size: 1.5rem;
+`;
--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { SinginLog } from "./SigninElements";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  return (
+    <>
+      <SinginLog>banko</SinginLog>
+
+      <SigninContainer>
+        <SigninHeader>Sign in to your account</SigninHeader>
+
+        <SigninLabel>Email</SigninLabel>
+        <SigninInp
+          placeholder="Enter your email address"
+          required
+          type="email"
+          autofocus
+          key="email"
+        ></SigninInp>
+        <SigninLabel>Password</SigninLabel>
+        <SigninInp
+          placeholder="Enter your email address"
+          required
+          type="password"
+          key="email"
+        ></SigninInp>
+      </SigninContainer>
+    </>
+  );
 };
 
 export default Signin;
