4302f73ecc003b0540356b82980879f527f942c5
y-emam <YasserEmam2002@gmail.com>
2024-05-23 21:57:47 +0300

    removed more unused imports

--- src/components/welcome/WelcomeElements.js
+++ src/components/welcome/WelcomeElements.js
@@ -1,6 +1,4 @@
 import styled from "styled-components";
-import { Link as LinkR } from "react-router-dom";
-import { Link as LinkS } from "react-scroll";
 import { MdKeyboardArrowRight, MdArrowForward } from "react-icons/md";
 
 export const WelcomeContainer = styled.div`
